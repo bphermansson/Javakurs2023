fdad19f1207da966cbd0c142b593e37d05060852
Patrik Hermansson <hermansson.patrik@gmail.com>
2023-05-25 12:16:36 +0200

    LabbProgrammeringForts - command line version working

--- LabbProgrammeringForts/src/Model.java
+++ LabbProgrammeringForts/src/Model.java
@@ -26,9 +26,10 @@ public class Model {
 	public void setValue(Integer key, Long value){
 		// Check if key is used
 		// Check if key & value is valid
+		System.out.println("Add value to memory");
 		
-		if (key == (int)key && key > 0) {		// Is key an integer above zero?
-			if (cache.containsKey(key) || key<1||value==null) {
+		if (key == (int)key && key >= 0 && value == (Long)value && value>=0) {		// Is key an integer above zero? Value a long above zero?
+			if (cache.containsKey(key) ) {
 				System.out.println("Key already exists! ");
 			}
 			else {
@@ -38,7 +39,9 @@ public class Model {
 	}
 	
 	private void addValueToMemory(Integer key, Long value){
+		System.out.println("Add: " + key + " - " + value);
 		cache.put(key, value);
+	    //System.out.println("Full list: " + cache);
 	}
 	public void clearMemory(){
 		cache.clear();
@@ -47,52 +50,68 @@ public class Model {
 	int res=2;
 	int sum=2;
 
-	//private long computePower(int power)  Ska vara private!
 	public long computePower(int power) {
-		//System.out.println("Loop" );
+		if(power>=0) {
+			return (computePowerInternal(power));
+		}
+		else {
+			System.out.println("Invalid power value: " + power);
+			return 0;
+		}
+	}
+	
+	private long computePowerInternal(int power) {
 		if(power>1) {
-			sum = sum * 2;
-			//System.out.println(sum + " " + power);			
+			sum = sum * 2;			
 			power--;
 			computePower(power);
+		}
+		else if (power == 1) {
+			// Special case, power of 1 doesn't do anything.
+			res = sum;
+			sum = 2;
+		}
+		else if (power == 0) {
+			// Special case, power of 0 is 1.
+			res = 1;
+			sum = 2;
 		}
 		else {
 			res = sum;
 			sum = 2;
-			System.out.println("res  " + res);						
 		}
 		return res; 
-
-			/*
-			while(power>0) {
-				res = res * res;
-				power--;
-				System.out.println("power:" + power);
-					computePower(power);	
-			}
-			System.out.println("power=0");
-		}
-		System.out.println("res:" + res);
-		System.out.println(summa(4));  
-        return res;
-*/
 	}
 	public long compute2Power(int power){
-		return 0;
-	}
-	private static int summa (int N) {
-	    // Hjälpvariabel
-	    int sum; 
-	    // Basfall
-	    if (N == 1) { 
-	        sum = 1;
-	    }
-	    else {
-	        // Metoden anropar sig själv --> Rekursivt anrop
-	        sum = N + summa(N - 1);  
-	    }
-	    //Returnera summan
-	    return sum; 
-	}
+		// Let power be the key, and the result the value
+		long key = 0, val = 0;
+		try {
+			isPowerLessThanZero(power);
+			System.out.println("Add computed value. Power value ok: " + power );
+			key = power;
 			
+			if (cache.containsKey((int)key) ) {
+				System.out.println("Value already exists!");
+				val = getValue((int)key);
+			}
+			else {
+				val = computePower(power);
+				System.out.println("New val: " + val + ", key: " + key);
+				setValue((int)key, val);
+			    System.out.println("Memory contains: " + cache);	
+			}
+			
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Error " + e );
+			System.exit(1);
+		}
+		return val;
+	}			
+	
+	private void isPowerLessThanZero(int power) {
+		if (power<0) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
--- LabbProgrammeringForts/src/Program.java
+++ LabbProgrammeringForts/src/Program.java
@@ -2,6 +2,7 @@
 public class Program {
 	public static void main(String[] args) {
 		Model myModel = new Model();
+		/*
 		try {
 			long h = myModel.getValue(2);
 		}
@@ -14,12 +15,22 @@ public class Program {
 		myModel.setValue(3, (long)23);
 		
 		long h = myModel.getValue(2);
-		System.out.println("Value: " + h);
-		
-		long power_sum = myModel.computePower(3);
-		System.out.println(power_sum);
-		System.out.println( myModel.computePower(5));
-		System.out.println( myModel.computePower(15));
+		System.out.println("Value from hashtable: " + h);		 
+		System.out.println("Power 3: " + myModel.computePower(3));	// 8
+		System.out.println("Power 5: " + myModel.computePower(5));	// 32
+		System.out.println( "Power -2: " + myModel.computePower(-2));	// 0
+		System.out.println("Power 15: " +  myModel.computePower(15));	// 32768
+		System.out.println( "Power 25: " + myModel.computePower(25)); // 33554432
+		System.out.println( "Power 0: " + myModel.computePower(0));	// 1
+		System.out.println( "Power 1: " + myModel.computePower(1));	// 2
+*/
+		//System.out.println( "compute2Power -1: " + myModel.compute2Power(-1));
+		System.out.println( "compute2Power 0 result: " + myModel.compute2Power(0));
+		System.out.println( "compute2Power 1 result: " + myModel.compute2Power(1));
+		System.out.println( "compute2Power 3 result: " + myModel.compute2Power(3));
+		System.out.println( "compute2Power 5 result: " + myModel.compute2Power(5));
+		System.out.println( "compute2Power 3 result: " + myModel.compute2Power(3));
+
 		
 	}
 }
