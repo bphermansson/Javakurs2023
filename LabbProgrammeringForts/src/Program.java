5012b2a9cf937abe65f866bf4b4ab802411afff3
Patrik Hermansson <hermansson.patrik@gmail.com>
2023-05-25 13:38:40 +0200

    LabbProgrammeringForts GUI klart

--- LabbProgrammeringForts/src/Program.java
+++ LabbProgrammeringForts/src/Program.java
@@ -1,7 +1,60 @@
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Program {
 	public static void main(String[] args) {
 		Model myModel = new Model();
+		
+		JFrame window = new JFrame();
+		window.setSize(new Dimension(600, 300));
+		window.setResizable(false);
+		window.setTitle("Cache memory - 2^n");
+		window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		window.setLocationRelativeTo(null);
+		
+		// Top panel with input textfield
+		JPanel inputPanel = new JPanel();
+			inputPanel.setPreferredSize(new Dimension(600, 40));
+		
+			final JTextField inputVal = new JTextField("0");
+			inputVal.setPreferredSize(new Dimension(150, 45));
+			inputPanel.add(inputVal, BorderLayout.CENTER);	
+			window.add(inputPanel, BorderLayout.NORTH);
+		
+		// Center panel with result label
+		 JLabel resultPanel= new JLabel("Result: ");
+		 	resultPanel.setPreferredSize(new Dimension(150, 100));
+		 	resultPanel.setText("Result: ");
+		 	resultPanel.setHorizontalAlignment(JLabel.CENTER);
+		 	resultPanel.setVerticalAlignment(JLabel.CENTER);
+		 	window.add(resultPanel);
+		
+		// Lower panel with buttons
+		JPanel btnPanel = new JPanel();
+			btnPanel.setPreferredSize(new Dimension(600, 45));
+			window.add(btnPanel, BorderLayout.SOUTH);
+	
+			JButton computeButton = new JButton("Compute 2^n");
+			computeButton.setPreferredSize(new Dimension(120, 40));
+			JButton clearButton = new JButton("Clear memory");
+			clearButton.setPreferredSize(new Dimension(120, 40));
+			JButton quitButton = new JButton("Quit");
+			quitButton.setPreferredSize(new Dimension(120, 40));
+		
+			btnPanel.add(computeButton, BorderLayout.EAST);
+			btnPanel.add(clearButton, BorderLayout.CENTER);
+			btnPanel.add(quitButton, BorderLayout.WEST);
+		
+		window.setVisible(true);
+
 		/*
 		try {
 			long h = myModel.getValue(2);
